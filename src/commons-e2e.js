06bf94e2cbcbe883661c2754925400ee0e839433
Tyler Biethman <tbiethman@users.noreply.github.com>
2023-07-27 11:10:23 -0500

    Initialize alias-ts-paths

---
+++ src/commons-e2e.js
@@ -0,0 +1,11 @@
+System.register([], function (exports_1, context_1) {
+    "use strict";
+    var forward;
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [],
+        execute: function () {
+            exports_1("forward", forward = true);
+        }
+    };
+});
