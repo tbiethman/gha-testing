40e6480c5583183d10adbbfe8e3bb91217e60c64
Tyler Biethman <tbiethman@users.noreply.github.com>
2023-07-27 11:10:49 -0500

    Initialize import-css-demo

---
+++ README.md
@@ -0,0 +1,12 @@
+# cypress-test-tiny
+
+> Tiny Cypress E2E test case
+
+Showing the DOM snapshots "jumping" around when doing time-traveling debugging if the styles are using `@import url` syntax
+
+```
+npm install
+npx cypress open
+```
+
+Hover over the "click" command in each test to see different behavior.
---
+++ cypress/integration/spec.js
@@ -0,0 +1,35 @@
+/// <reference types="cypress" />
+describe('page', () => {
+  it('works without jumps', () => {
+    cy.visit('index.html')
+    cy.get('button').click()
+  })
+
+  it('works without jumps using CSS resource link', () => {
+    cy.visit('index-with-css-link.html')
+    cy.get('button').click()
+  })
+
+  it('works without jumps using CSS resource link even if it is delayed', () => {
+    cy.intercept('reset.css', {
+      fixture: 'reset.css',
+      delay: 500,
+    })
+    cy.visit('index-with-css-link.html')
+    cy.get('button').click()
+  })
+
+  it('DOM snapshots jump', () => {
+    cy.visit('index-with-css-import.html')
+    cy.get('button').click()
+  })
+
+  it('DOM snapshots jump with CSS resource delay', () => {
+    cy.intercept('reset.css', {
+      fixture: 'reset.css',
+      delay: 500,
+    })
+    cy.visit('index-with-css-import.html')
+    cy.get('button').click()
+  })
+})
